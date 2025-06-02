bed07def9b3a9ee19de4c811ff1c302f282852cd
ishan2206 <ishaan2206@gmail.com>
2025-06-02 10:54:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,13 +14,12 @@ const RdserviceFingerprintscanner = NativeModules.RdserviceFingerprintscanner
         get() {
           throw new Error(LINKING_ERROR);
         },
-      }
+      },
     );
 
 export const AVAILABLE_PACKAGES = {
   Secugen: 'com.secugen.rdservice',
   Morpho: 'com.scl.rdservice',
-  Mantra: 'com.mantra.rdservice',
   Startek_FM220: 'com.acpl.registersdk',
   Gemalto_3M_Cogent_CSD200: 'com.rd.gemalto.com.rdserviceapp',
   Integra: 'com.integra.registered.device',
@@ -29,7 +28,6 @@ export const AVAILABLE_PACKAGES = {
   Tatvik_TMF20: 'com.tatvik.bio.tmf20',
   Evolute: 'com.evolute.rdservice',
   PB510: 'com.precision.pb510.rdservice',
-  MIS100V2_by_Mantra: 'com.mantra.mis100v2.rdservice',
   NEXT_Biometrics_NB3023: 'com.nextbiometrics.rdservice',
   IriTech_IriShield: 'com.iritech.rdservice',
   Evolute_IRIS: 'com.evolute.iris.rdservice',
@@ -67,106 +65,24 @@ export function getDeviceInfo() {
 
 export function isDriverFound(packageName) {
   return new Promise((resolve, reject) => {
-    if (packageName) {
-      RdserviceFingerprintscanner.isDriverFound(packageName)
-        .then((res) => {
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    } else {
-      reject('Package Name cannot be empty');
-    }
+    reject('Err:4587 - Not supported');
   });
 }
 
-export function openFingerPrintScanner(
-  packageName,
-  pidOptions = DEFAULT_PID_OPTIONS
-) {
+export function openFingerPrintScanner(packageName, pidOptions = DEFAULT_PID_OPTIONS) {
   return new Promise((resolve, reject) => {
-    if (packageName) {
-      RdserviceFingerprintscanner.openFingerPrintScanner(
-        packageName,
-        pidOptions
-      )
-        .then((res) => {
-          if (res.pidDataJsonString) {
-            const resObj = {
-              pidDataJson: JSON.parse(res.pidDataJsonString),
-              pidDataXML: res.pidDataXML,
-              rdServicePackage: res.rdServicePackage,
-              status: res.status,
-              errInfo: res.errInfo,
-              errorCode: parseInt(res.errorCode),
-              message: res.message,
-            };
-            resolve(resObj);
-          } else {
-            resolve(res);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    } else {
-      reject('Package Name cannot be empty');
-    }
+    reject('Err:4557 - Not supported');
   });
 }
 
-export function openFaceAuth(
- transactionId = Math.random().toString(36).substring(7)
-) {
+export function openFaceAuth(transactionId = Math.random().toString(36).substring(7)) {
   return new Promise((resolve, reject) => {
-    if (transactionId ) {
-      RdserviceFingerprintscanner.openFaceAuth(
-        transactionId
-      )
-        .then((res) => {
-          
-          if (res.pidDataJsonString) {
-            const resObj = {
-              pidDataJson: JSON.parse(res.pidDataJsonString),
-              pidDataXML: res.pidDataXML,
-              rdServicePackage: res.rdServicePackage,
-              status: res.status,
-              errInfo: res.errInfo,
-              errorCode: parseInt(res.errorCode),
-              message: res.message,
-            };
-            resolve(resObj);
-          } else {
-            resolve(res);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    } else {
-      reject('Package Name cannot be empty');
-    }
+    reject('Err:4542 - Not supported');
   });
 }
 
 export function captureFinger(pidOptions = DEFAULT_PID_OPTIONS) {
   return new Promise((resolve, reject) => {
-    RdserviceFingerprintscanner.captureFinger(pidOptions)
-      .then((res) => {
-        const resObj = {
-          pidDataJson: JSON.parse(res.pidDataJsonString),
-          pidDataXML: res.pidDataXML,
-          rdServicePackage: res.rdServicePackage,
-          status: res.status,
-          errInfo: res.errInfo,
-          errorCode: parseInt(res.errorCode),
-          message: res.message,
-        };
-        resolve(resObj);
-      })
-      .catch((err) => {
-        reject(err);
-      });
+    reject('Err:4598 - Not supported');
   });
 }
